2128b1f542be800c88d75fa0b298f9c3fb3bc184
dinhhoanganh <dinhhoanganh@vnpt.vn>
2024-07-01 11:13:58 +0700

    xong hạ tầng core

---
+++ src/TeduBlog.Data/SeedWorks/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using TeduBlog.Core.Repositories;
+using TeduBlog.Core.SeedWorks;
+using TeduBlog.Data.Repositories;
+
+namespace TeduBlog.Data.SeedWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TeduBlogContext _context;
+        private readonly IMapper _mapper;
+
+        public UnitOfWork(TeduBlogContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            Posts = new PostRepository(context, mapper);
+        }
+
+        public IPostRepository Posts { get; private set; }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
